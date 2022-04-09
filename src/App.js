30b3dab4df1c15700f74a5a8ee59462e686a088e
Sherry90 <hhlsdkm@gmail.com>
2022-04-09 19:08:23 +0900

    set props & data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import ExpenseItem from "./components/ExpenseItem";
+import ExpenseData from "./data/ExpenseData";
 
-function App() {
+const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
-        <ExpenseItem></ExpenseItem>
+        <ExpenseItem title={ExpenseData[0].title} amount={ExpenseData[0].amount} date={ExpenseData[0].date.toISOString()}></ExpenseItem>
+        <ExpenseItem title={ExpenseData[1].title} amount={ExpenseData[1].amount} date={ExpenseData[1].date.toISOString()}></ExpenseItem>
+        <ExpenseItem title={ExpenseData[2].title} amount={ExpenseData[2].amount} date={ExpenseData[2].date.toISOString()}></ExpenseItem>
+        <ExpenseItem title={ExpenseData[3].title} amount={ExpenseData[3].amount} date={ExpenseData[3].date.toISOString()}></ExpenseItem>
     </div>
   );
 }
