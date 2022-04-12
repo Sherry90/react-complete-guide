87599dbc77f4ab0e8ce64fade874727e56227bff
Sherry90 <hhlsdkm@gmail.com>
2022-04-12 16:56:05 +0900

    split date component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
-        <ExpenseItem title={ExpenseData[0].title} amount={ExpenseData[0].amount} date={ExpenseData[0].date}></ExpenseItem>
-        <ExpenseItem title={ExpenseData[1].title} amount={ExpenseData[1].amount} date={ExpenseData[1].date}></ExpenseItem>
-        <ExpenseItem title={ExpenseData[2].title} amount={ExpenseData[2].amount} date={ExpenseData[2].date}></ExpenseItem>
-        <ExpenseItem title={ExpenseData[3].title} amount={ExpenseData[3].amount} date={ExpenseData[3].date}></ExpenseItem>
+        <ExpenseItem title={ExpenseData[0].title} amount={ExpenseData[0].amount} date={ExpenseData[0].date} />
+        <ExpenseItem title={ExpenseData[1].title} amount={ExpenseData[1].amount} date={ExpenseData[1].date} />
+        <ExpenseItem title={ExpenseData[2].title} amount={ExpenseData[2].amount} date={ExpenseData[2].date} />
+        <ExpenseItem title={ExpenseData[3].title} amount={ExpenseData[3].amount} date={ExpenseData[3].date} />
     </div>
   );
 }
