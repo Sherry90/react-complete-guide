a41638753cfbe20b2f73046ff2e138b1a5cdc8e3
Sherry90 <hhlsdkm@gmail.com>
2022-04-12 17:26:04 +0900

    delete space

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,8 +2,6 @@ import ExpenseDate from "./ExpenseDate";
 import './ExpensiveItem.css';
 
 const ExpenseItem = (props) => {
-
-
     return (
         <div className ="expense-item" >
             <ExpenseDate date = {props.date} />
