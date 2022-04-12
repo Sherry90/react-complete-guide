e486bdf018c3967093a2adeb014fce57aea6f87c
Sherry90 <hhlsdkm@gmail.com>
2022-04-12 21:17:12 +0900

    split Expenses Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 import ExpenseData from "./data/ExpenseData";
 
 const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
-        <ExpenseItem title={ExpenseData[0].title} amount={ExpenseData[0].amount} date={ExpenseData[0].date} />
-        <ExpenseItem title={ExpenseData[1].title} amount={ExpenseData[1].amount} date={ExpenseData[1].date} />
-        <ExpenseItem title={ExpenseData[2].title} amount={ExpenseData[2].amount} date={ExpenseData[2].date} />
-        <ExpenseItem title={ExpenseData[3].title} amount={ExpenseData[3].amount} date={ExpenseData[3].date} />
+      <Expenses item={ExpenseData} />
     </div>
   );
 }
