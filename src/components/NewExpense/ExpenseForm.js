e4dee7e1ab7e020acee8b0a32cfd9291839b42af
Sherry90 <hhlsdkm@gmail.com>
2022-04-20 20:39:40 +0900

    1 component, single state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,14 +4,40 @@ import './ExpenseForm.css'
 
 
 const ExpenseForm = () => {
+    /*
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
-
+    */
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount:'',
+        enteredDate:''
+    });
+    /*
     const titleChangeHandler = (event) => setEnteredTitle(event.target.value);
     const amountChangeHandler = (event) => setEnteredAmount(event.target.value);
     const dateChangeHandler = (event) => setEnteredDate(event.target.value);
+    */
 
+    const titleChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value,
+        });
+    }
+    const amountChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value,
+        });
+    }
+    const dateChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value,
+        });
+    }
     return <form>
         <div className="new-expense__controls">
             <div className="new-expense__control">
