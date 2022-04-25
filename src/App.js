e47413296652828e6d47312d3dab7e44ee480f87
윤창현 <hhlsdkm@gmail.com>
2022-04-25 15:42:14 +0900

    add expense data array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 import ExpenseData from "./data/ExpenseData";
 
 const App = () => {
+    const [expenses, setExepnses] = useState(ExpenseData);
+
     const addExpenseHandler = expense => {
-        console.log('In App');
-        console.log(expense);
+        setExepnses(prevExpenses => {
+            return [expense, ...prevExpenses];
+        });
     };
 
     return (
         <div>
             <NewExpense onAddExpense={addExpenseHandler} />
-            <Expenses item={ExpenseData}/>
+            <Expenses item={expenses}/>
         </div>
     );
 }
