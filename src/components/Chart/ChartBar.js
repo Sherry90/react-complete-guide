2f5de6dd19b2b40266eeefa045e479ac9de37d2a
윤창현 <hhlsdkm@gmail.com>
2022-04-28 09:15:50 +0900

    chartDataPoints.map

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -11,9 +11,9 @@ const ChartBar = (props) => {
 
     return <div className="chart-bar">
         <div className="chart-bar__inner">
-            <div className="chart-bar__fill"></div>
+            <div className="chart-bar__fill" style={{height: barFillHeight}}></div>
         </div>
-        <div className="chart-bar__label"></div>
+        <div className="chart-bar__label">{props.label}</div>
     </div>
 }
 
